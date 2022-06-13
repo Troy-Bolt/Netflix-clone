792f4a0d1a1b9c603985fef392e9edeca4ffb8bd
Troy <troy@gigrilla.com>
2022-06-13 08:07:50 +0100

    gradient background

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
     }, [])
 
   return (
-    <header className={`${isScrolled && 'bg-purple-500'}`}>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img 
             src="https://rb.gy/ulxxee"
